6272d1b3e240fe08322805b993521525f21103f2
Pedro-MM1 <zimagamesmeireles@gmail.com>
2026-04-02 00:47:09 -0300

    fix: Firebase Auth persistence condicional web vs native

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,7 +1,8 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { browserLocalPersistence, getAuth, getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
 const firebaseConfig = {
     apiKey: "AIzaSyC9aNuLn_oW6xc2w23CsLfTlzubJe2a_dI",
@@ -15,10 +16,12 @@ const firebaseConfig = {
 const alreadyInitialized = getApps().length > 0;
 export const app = alreadyInitialized ? getApp() : initializeApp(firebaseConfig);
 
+const persistence = Platform.OS === "web"
+    ? browserLocalPersistence
+    : getReactNativePersistence(ReactNativeAsyncStorage);
+
 export const auth = alreadyInitialized
     ? getAuth(app)
-    : initializeAuth(app, {
-        persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-    });
+    : initializeAuth(app, { persistence });
 
 export const db = getFirestore(app);
